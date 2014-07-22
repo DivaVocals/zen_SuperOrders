e945ed710d4ea03baede3e2e07554827d2d9fe17
retched <retched@users.noreply.github.com>
2014-07-21 21:20:31 -0400

    Missing Language define
    super_batch_pages never had a define for 'ERROR_NO_ORDERS'

---
+++ 1_Base_Files/YOUR_ADMIN/includes/languages/english/super_batch_pages.php
@@ -0,0 +1,26 @@
+<?php
+/*
+//////////////////////////////////////////////////////////////////////////
+//  SUPER ORDERS v3.0                                               	//
+//                                                                  	//
+//  Based on Super Order 2.0                                        	//
+//  By Frank Koehl - PM: BlindSide (original author)                	//
+//                                                                  	//
+//  Super Orders Updated by:						//
+//  ~ JT of GTICustom							//
+//  ~ C Jones Over the Hill Web Consulting (http://overthehillweb.com)	//
+//  ~ Loose Chicken Software Development, david@loosechicken.com	//
+//                                                      		//
+//  Powered by Zen-Cart (www.zen-cart.com)              		//
+//  Portions Copyright (c) 2005 The Zen-Cart Team       		//
+//                                                     			//
+//  Released under the GNU General Public License       		//
+//  available at www.zen-cart.com/license/2_0.txt       		//
+//  or see "license.txt" in the downloaded zip          		//
+//////////////////////////////////////////////////////////////////////////
+//  DESCRIPTION: Language file definitions for super_batch_status.php	//
+//////////////////////////////////////////////////////////////////////////
+// $Id: super_batch_status.php v 2010-10-24 $
+*/
+
+define('ERROR_NO_ORDERS', 'Error: No orders selected!');
